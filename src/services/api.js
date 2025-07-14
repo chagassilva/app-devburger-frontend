8f87863f853dd8b3e77116f512d5567ab0883941
chagassilva <chagassilvati@gmail.com>
2025-07-14 00:48:17 -0300

    Atualizando a portas

--- src/services/api.js
+++ src/services/api.js
@@ -13,7 +13,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3002"
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3001"
 });
 
 
